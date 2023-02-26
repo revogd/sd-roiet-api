a7ba183a890beb59af85eb5bc2cd751498af6621
revogd <88536930+revogd@users.noreply.github.com>
2023-02-27 01:56:50 +0700

    Update query

--- index.js
+++ index.js
@@ -36,8 +36,7 @@ app.get('/soldiers', (req, res) => {
 app.get('/soldier/:pid', (req, res) => {    
     db.query("SELECT เลขประจำตัวประชาชน,ชื่อ,นามสกุล,วัน เกิด,เดือน เกิด,เกิด,ตำบล,อำเภอ,จังหวัด FROM TB_Recuit WHERE เลขประจำตัวประชาชน=?",
     [req.params.pid],
-    (err, result) => {
-        
+    (err, result) => {      
         
         if (err) {
             console.log(err);
