aedb1caf525a00eea7a0cedd86d00e483a4adb99
AthisakPan <88536930+revogd@users.noreply.github.com>
2023-06-06 01:01:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get('/soldiers', (req, res) => {
 });
 
 app.get('/soldier/:id', (req, res) => {
-    const id = parseInt(req.params.id);
+    const id = req.params.id;
     db.query('SELECT เลขประจำตัวประชาชน, ชื่อ, นามสกุล, เกิด, อำเภอ, จังหวัด FROM TB_Recuit WHERE ID = ?', 
     [id],
     (err, result) => {       
